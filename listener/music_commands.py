4c29252403901b9bcad27577000896d4f3ebb9f2
MadeYoga <vngla21@gmail.com>
2019-01-22 21:49:49 +0630

    update music listener&core

--- listener/music_commands.py
+++ listener/music_commands.py
@@ -1,55 +1,8 @@
 import asyncio
-
+# import youtube_dl
 import discord
-import youtube_dl
-
 from discord.ext import commands
-
-# Suppress noise about console usage from errors
-youtube_dl.utils.bug_reports_message = lambda: ''
-
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
-}
-
-ffmpeg_options = {
-    'options': '-vn'
-}
-
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-
-
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-
-        self.data = data
-
-        self.title = data.get('title')
-        self.url = data.get('url')
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
-
+from .core.music import YTDLSource
 
 class Music:
     def __init__(self, bot):
@@ -59,9 +12,10 @@ class Music:
     async def join(self, ctx, *, channel: discord.VoiceChannel):
         """Joins a voice channel"""
 
+        # if already connected to voice channel, then move
         if ctx.voice_client is not None:
             return await ctx.voice_client.move_to(channel)
-
+        # if not connected yet, then connect
         await channel.connect()
 
     @commands.command()
@@ -100,7 +54,7 @@ class Music:
         if ctx.voice_client is None:
             return await ctx.send("Not connected to a voice channel.")
 
-        ctx.voice_client.source.volume = volume
+        ctx.voice_client.source.volume = float(volume/100)
         await ctx.send("Changed volume to {}%".format(volume))
 
     @commands.command()
@@ -108,6 +62,15 @@ class Music:
         """Stops and disconnects the bot from voice"""
 
         await ctx.voice_client.disconnect()
+
+    @commands.command()
+    async def summon(self, ctx):
+        """Force the bot to join author's voice channel"""
+
+        if ctx.author.voice:
+            await ctx.author.voice.channel.connect()
+        else:
+            await ctx.send("You are not connected to a voice channel.")
 
     @play.before_invoke
     @yt.before_invoke
@@ -122,4 +85,6 @@ class Music:
         elif ctx.voice_client.is_playing():
             ctx.voice_client.stop()
 
-bot.add_cog(Music(bot))
+def setup(bot):
+    bot.add_cog(Music(bot))
+    print('Music is loaded')
